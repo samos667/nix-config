d48b84b649a25d45e9b1b68af0f4ce94de296eab
samos667 <samos667@k8s-ekip.cc>
2024-02-11 20:19:56 +0100

    sync

--- modules/default.nix
+++ modules/default.nix
@@ -4,7 +4,6 @@
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.sam = {
-    shell = pkgs.nushell;
     isNormalUser = true;
     openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAJzb59KOgc9ldoJgsvH7eiE5BkIQQN059Cst0QYlFRN sam@workstation.k8s-ekip.cc"];
     extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
