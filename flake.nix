da24ee1e7976012a71abed96b43c2263ba4f4720
samos667 <samos667@k8s-ekip.cc>
2024-02-11 06:16:55 +0100

    sync

--- flake.nix
+++ flake.nix
@@ -64,6 +64,21 @@
         ];
 
       };
+      "mc" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          ./hosts/mc
+	        home-manager.nixosModules.home-manager
+	        {
+	          home-manager.useGlobalPkgs = true;
+	          home-manager.useUserPackages = true;
+	          home-manager.extraSpecialArgs = inputs;
+	          home-manager.users.sam = import ./home/ctemplate/home.nix;
+	        }
+        ];
+
+      };
     };
   };
 }
