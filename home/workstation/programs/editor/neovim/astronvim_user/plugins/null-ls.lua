b829d33c59f724a3b7ed94b1f0de2b2b40336059
samos667 <samos667@k8s-ekip.cc>
2024-03-23 03:07:55 +0100

    fix null-ls

--- home/workstation/programs/editor/neovim/astronvim_user/plugins/null-ls.lua
+++ home/workstation/programs/editor/neovim/astronvim_user/plugins/null-ls.lua
@@ -1,44 +1,34 @@
 return {
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function(_, opts)
-      local null_ls = require "null-ls"
-      local code_actions = null_ls.builtins.code_actions
-      local diagnostics = null_ls.builtins.diagnostics
-      local formatting = null_ls.builtins.formatting
-      local hover = null_ls.builtins.hover
-      local completion = null_ls.builtins.completion
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
-      if type(opts.sources) == "table" then
-        vim.list_extend(opts.sources, {
-          -- Common Code Actions
-          code_actions.gitsigns,
-          -- common refactoring actions based off the Refactoring book by Martin Fowler
-          code_actions.refactoring,
-          code_actions.shellcheck,
-          code_actions.proselint,    -- English prose linter
-          code_actions.statix,       -- Lints and suggestions for Nix.
-
-          -- Diagnostic
-          diagnostics.actionlint, -- GitHub Actions workflow syntax checking
-          diagnostics.deadnix,    -- Scan Nix files for dead code.
-
-          -- Formatting
-          formatting.prettier,                        -- js/ts/vue/css/html/json/... formatter
-          diagnostics.hadolint,                       -- Dockerfile linter
-          formatting.black,                           -- Python formatter
-          formatting.ruff,                            -- extremely fast Python linter
-          formatting.shfmt,                           -- Shell formatter
-          formatting.taplo,                           -- TOML formatteautoindentr
-          formatting.terraform_fmt,                   -- Terraform formatter
-          formatting.stylua,                          -- Lua formatter
-          formatting.alejandra,                       -- Nix formatter
-          formatting.sqlfluff.with {                  -- SQL formatter
-            extra_args = { "--dialect", "postgres" }, -- change to your dialect
-          },
-          formatting.nginx_beautifier,                -- Nginx formatter
-        })
-      end
-    end,
-  },
+  "jose-elias-alvarez/null-ls.nvim",
+  opts = function(_, config)
+    local null_ls = require "null-ls"
+    -- Check supported formatters and linters
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/
+    config.sources = {
+      -- Common Code Actions
+      null_ls.builtins.code_actions.gitsigns,
+      -- common refactoring actions based off the Refactoring book by Martin Fowler
+      null_ls.builtins.code_actions.refactoring,
+      null_ls.builtins.code_actions.shellcheck,
+      null_ls.builtins.code_actions.proselint,      -- English prose linter
+      null_ls.builtins.code_actions.statix,         -- Lints and suggestions for Nix.
+      -- Diagnostic
+      null_ls.builtins.diagnostics.actionlint,      -- GitHub Actions workflow syntax checking
+      null_ls.builtins.diagnostics.deadnix,         -- Scan Nix files for dead code.
+      -- formatting
+      null_ls.builtins.formatting.prettier,         -- js/ts/vue/css/html/json/... formatter
+      null_ls.builtins.diagnostics.hadolint,        -- Dockerfile linter
+      null_ls.builtins.formatting.black,            -- Python formatter
+      null_ls.builtins.formatting.ruff,             -- extremely fast Python linter
+      null_ls.builtins.formatting.shfmt,            -- Shell formatter
+      null_ls.builtins.formatting.terraform_fmt,    -- Terraform formatter
+      null_ls.builtins.formatting.stylua,           -- Lua formatter
+      null_ls.builtins.formatting.alejandra,        -- Nix formatter
+      null_ls.builtins.formatting.sqlfluff.with {   -- SQL formatter
+        extra_args = { "--dialect", "postgres" },   -- change to your dialect
+      },
+      null_ls.builtins.formatting.nginx_beautifier, -- Nginx formatter
+    }
+    return config
+  end,
 }
