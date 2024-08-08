d5c26e3cb284488572db9000e6d0ccff4d2e7467
samos667 <samos667@setur.cc>
2024-08-08 23:50:19 +0200

    feat: follow hyprland inputs for opengl driver

--- modules/configuration/system/nvidia.nix
+++ modules/configuration/system/nvidia.nix
@@ -1,9 +1,19 @@
 # Install the proprietary Nvidia GPU drivers
-{ config, lib, ... }: {
+{ config, lib, inputs, pkgs, ... }:
+let
+  pkgs-unstable =
+    inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+in {
   # Nvidia drivers are unfree software, if we want to use them, we must allow unfree
   nixpkgs.config.allowUnfree = true;
 
-  hardware.opengl.enable = true;
+  hardware.opengl = {
+    package = pkgs-unstable.mesa.drivers;
+
+    # if you also want 32-bit support (e.g for Steam)
+    driSupport32Bit = true;
+    package32 = pkgs-unstable.pkgsi686Linux.mesa.drivers;
+  };
   # These are the correct drivers for modern Nvidia GPUs, this may need to be changed for older cards
   hardware.nvidia.package =
     lib.mkDefault config.boot.kernelPackages.nvidiaPackages.stable;
