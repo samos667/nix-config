bea3f34a3d50e340b370a79920f30d6a94d8a64c
samos667 <samos667@setur.cc>
2024-08-08 23:50:48 +0200

    feat: use cachix for hyprland packages

--- modules/configuration/gui/hypr/hyprland.nix
+++ modules/configuration/gui/hypr/hyprland.nix
@@ -4,4 +4,10 @@
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
   };
   security.pam.services.swaylock = { };
+
+  nix.settings = {
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys =
+      [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
 }
